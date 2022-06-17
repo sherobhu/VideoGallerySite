c75a9ce2c3d008338aa21af1c52c7cd7f9679687
Bhuvana <85592657+sherobhu@users.noreply.github.com>
2022-06-17 18:30:36 +0530

    initial commit

---
+++ last/script.js
@@ -0,0 +1,13 @@
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+   };
+});
